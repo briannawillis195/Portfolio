0f6a060e4e7eef988823d156d2bbfef03526698f
brianna willis <brianna.willis195@gmail.com>
2024-01-30 19:23:47 -0500

    new portfolio commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    // Smooth scrolling for navbar links
+    $('.navbar-nav a').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
